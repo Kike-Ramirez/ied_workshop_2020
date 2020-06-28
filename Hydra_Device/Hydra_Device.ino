7ec9de1bd025d5890ab5f5e12aa8aa035b57983e
Kike Ramírez <eramirez@barbaraiot.com>
2020-06-28 14:53:37 +0200

    Led tests added & visual design for led added

--- Hydra_Device/Hydra_Device.ino
+++ Hydra_Device/Hydra_Device.ino
@@ -1,6 +1,7 @@
 // LED will blink when in config mode
 
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <FastLED.h>
 
 //for LED status
 #include <Ticker.h>
@@ -12,13 +13,14 @@ Ticker ticker;
 
 int LED = LED_BUILTIN;
 
-//NEOPIXEL
-#include <Adafruit_NeoPixel.h>
 
-#define LED_PIN  D1
-#define LED_COUNT 1
+//FastLed Setup
 
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+#define REDPIN   D3
+#define GREENPIN D2
+#define BLUEPIN  D1
+
+
 
 // Time period of fading in millisecs
 #define PERIOD 2000
@@ -27,21 +29,34 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 // No Phase
 #define PHASE 0
 // Offset of the sine wave
-#define OFFSET 128
+#define OFFSET 128.0
 // Amplitude of the sine wave
-#define AMPLITUDE 127
+#define AMPLITUDE 127.0
 
 // Used to generate time for the cos wave
 unsigned long timer = 0;
+
+void showAnalogRGB( const CRGB& rgb)
+{
+  analogWrite(REDPIN,   rgb.r );
+  analogWrite(GREENPIN, rgb.g ); //rgb.g
+  analogWrite(BLUEPIN,  0 );
+}
+
+
 
 void tick()
 {
   //toggle state
   //digitalWrite(LED, !digitalRead(LED));     // set pin to the opposite state
-      timer = millis(); // updating time
-    int ledValue = OFFSET + AMPLITUDE*(cos((OMEGA*timer)+PHASE));
-    
-    strip.setPixelColor(0, ledValue);
+
+
+  float ledValue = OFFSET + AMPLITUDE * (cos((OMEGA * millis()) + PHASE));
+
+  showAnalogRGB(CHSV( ledValue, 255, 255));
+
+  Serial.println(ledValue);
+
 }
 
 //gets called when WiFiManager enters configuration mode
@@ -51,18 +66,21 @@ void configModeCallback (WiFiManager *myWiFiManager) {
   //if you used auto generated SSID, print it
   Serial.println(myWiFiManager->getConfigPortalSSID());
   //entered config mode, make led toggle faster
-  ticker.attach(0.2, tick);
+  ticker.attach(0.03, tick);
 }
 
 void setup() {
+
+  pinMode(REDPIN,   OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN,  OUTPUT);
+
   WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
   // put your setup code here, to run once:
   Serial.begin(115200);
-  
-  //set led pin as output
-  pinMode(LED, OUTPUT);
+
   // start ticker with 0.5 because we start in AP mode and try to connect
-  ticker.attach(0.6, tick);
+  //ticker.attach(0.6, tick);
 
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
@@ -86,9 +104,7 @@ void setup() {
 
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
-  ticker.detach();
-  //keep LED on
-  digitalWrite(LED, LOW);
+  //ticker.detach();
 }
 
 void loop() {
